ccc17847c90f37e1bf18c8398e646ccdbad32ef2
sureshkmahar <sureshmahar@outlook.com>
2019-07-25 22:18:50 +0530

    modified the WebLayer.java

--- springboot-2-rest-testing/src/test/java/com/athena/springboot/WebLayerTest.java
+++ springboot-2-rest-testing/src/test/java/com/athena/springboot/WebLayerTest.java
@@ -9,12 +9,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class WebLayerTest {
 
     @Autowired
@@ -22,7 +24,8 @@ public class WebLayerTest {
 
     @Test
     public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello World")));
+        this.mockMvc.perform(get("/")).andDo(print()).
+        andExpect(status().isOk())
+       .andExpect(content().string(containsString("Hello World")));
     }
 }
